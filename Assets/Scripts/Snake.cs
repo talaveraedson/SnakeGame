4f12b208a205d55472a90a59368a2d8444f3d687
talaveraedson <st2347@utr.edu.mx>
2022-03-04 16:03:42 -0600

    update commit

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -10,18 +10,6 @@ public class Snake : MonoBehaviour
 
     #region Properties
 
-    /// <summary>
-
-    /// Varible tipo enum que asigna a las variables left, right,
-
-    ///up y down, valores constantes que no cambiaran en el transcurso de
-
-    ///la ejecucion del programa
-
-    /// </summary>
-
-    //Estados o movimientos que puede tomar el player
-
     enum Movements
     {
 
@@ -35,50 +23,39 @@ public class Snake : MonoBehaviour
 
     }
 
-    //Declaramos una variable de tipo Movements que se llama movement
-
-    //para almacenar el movimiento que el player realice desde el teclado
 
     Movements _movement;
 
-    //declaramos la variable frameRate que seria nuestra variable para tener
-
-    //el tiempo que se estaria ejecutando cada frame, autodemoninado en Unity
-
-    //como time.Deltatime se ejecuata cada 200 milisegundos
 
     public float _frameRate = 0.2f;
 
-    //declaramos la variable nextStep para almacenar la distancia que se moveria por cada frameRate
-
-    //la serpiente en este caso avanzaria cada 16 px
 
     public float _nextStep = 0.16f;
 
-    //Declaramos una variable de tipo Vector3 que almacene la ultima posicion de la serpiente
 
     Vector3 _lastPos;
 
     public BoxCollider2D gridArea;
+
+    public List<Transform> _body = new List<Transform>();
+
+    public Transform _bodyPrefab;
+
+    public Transform _tailPrefab;
+
 
 
     #endregion
 
     #region Start
 
-    // Start is called before the first frame update
-
     void Start()
 
     {
-
-        //Invocamos al metodo Move en start para que se instancien las variables de
-
-        //movement, junto con frameRate y nextStep
-
-        //usamos el comando InvokeRepeating("MethodName", time.deltaTime,time.deltaTime)
-        
         RandomizePosition();
+
+        transform.localRotation = Quaternion.Euler(0, 0, -90);
+        _body[0].rotation = Quaternion.Euler(0, 0, 90);
         InvokeRepeating("Move", _frameRate, _frameRate);
 
     }
@@ -87,31 +64,20 @@ public class Snake : MonoBehaviour
 
     #region Methods
 
-    //Declaramos el metodo Move en este caso no regresa nada, solo revisa la ultima posicion de la serpiente
 
     void Move()
     {
 
-        //que esta en la variable lastPos y se le asigna la propiedad transform.position
 
         _lastPos = transform.position;
 
-        //Declaramos una varibale local nextPos de tipo Vector3 que inicialmente tomara los valores de 0 para
-
-        //la posicion del vector
 
         Vector3 nextPos = Vector3.zero;
 
-        //comparamos el valor almacenado en la variable movement y la comparamos con alguna de las
-
-        //acciones realizadas en la variable enum Movements
 
         if (_movement == Movements.up)
         {
 
-            //Si cumple con algunas de las acciones asignamos a la variable nextPos los valores del objeto
-
-            //Vector3.movimiento en este caso hacia arriba
 
             nextPos = Vector3.up;
 
@@ -139,22 +105,22 @@ public class Snake : MonoBehaviour
 
             nextPos = Vector3.right;
 
-        }
-
-        //ya que esta actualizado la variable con el movimiento que desea realizar el player
-
-        //incrementamos el valor de nextPos multiplicando por el valor que avanza cada frame
-
-        //que esta alamcenado en la variable nextStep
+        } 
 
         nextPos *= _nextStep;
-
-        //ahora ese valor de nextPos se lo asignamos a la propiedad transform.position para que actualice
-
-        //los nuevos valores del vector postion en el trandform del Inspector
-
         transform.position += nextPos;
+        MoveBody();
 
+    }
+
+    public void MoveBody()
+    {
+        for (int i = 0; i < _body.Count; i++)
+        {
+            Vector3 temp = _body[i].position;
+            _body[i].position = _lastPos;
+            _lastPos = temp;
+        }
     }
 
 
@@ -197,8 +163,8 @@ public class Snake : MonoBehaviour
         }
         else if (collider.CompareTag("Apple"))
         {
-            RandomizePosition();
-            print("Eat the Apple");
+            _body.Add(Instantiate(_bodyPrefab, _body[_body.Count-1].position,Quaternion.identity).transform);
+            print("You ate the Apple");
         }
         
     }
